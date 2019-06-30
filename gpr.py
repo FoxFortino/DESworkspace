38b13a38a8ea74267a0b4bbac29432e6def1d2cc
Austin Fox Fortino <fortino@sas.upenn.edu>
2019-06-29 20:53:12 -0400

    Update

---
+++ gpr.py
@@ -0,0 +1,235 @@
+from time import time
+import numpy as np
+import astropy.units as u
+import astropy.constants as c
+import astropy.io.fits as pf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+import gpr
+import mcmc
+import forAustin as fa
+
+class GPR(object):
+    def extract_exposure(self):
+        """Extracts all data from a specified exposure (self.nExposure). Currently only supports extracting one exposure's worth of data."""
+        if self.verbose: print("Extracting exposure from fits file...")
+        self.fits = pf.open(self.datafile)
+        if self.verbose: self.fits.info(); print()
+        self.exposure = fa.getExposure(self.fits, self.nExposure, polyOrder=3)
+    
+    def extract_data(self):
+        """Extract star positions, residuals, and measurement error from one exposure."""
+        if self.verbose: print("Extracting exposure data...")
+        ind_hasGaia = np.where(self.exposure['hasGaia'])[0]
+        u = np.take(self.exposure['u'], ind_hasGaia)
+        v = np.take(self.exposure['v'], ind_hasGaia)
+        dx = np.take(self.exposure['dx'], ind_hasGaia)
+        dy = np.take(self.exposure['dy'], ind_hasGaia)
+        self.E = np.take(self.exposure['measErr'], ind_hasGaia)
+        
+        # Extract only data in a certain region of the exposure as specified by self.sample.
+        if self.sample is not None:
+            ind_u = np.logical_and(u >= self.sample[0], u <= self.sample[1])
+            ind_v = np.logical_and(v >= self.sample[2], v <= self.sample[3])
+            ind_sample = np.where(np.logical_and(ind_u, ind_v))[0]
+            u = np.take(u, ind_sample, axis=0)
+            v = np.take(v, ind_sample, axis=0)
+            dx = np.take(dx, ind_sample, axis=0)
+            dy = np.take(dy, ind_sample, axis=0)
+            self.E = np.take(self.E, ind_sample)
+        self.X = np.vstack((u, v)).T
+        self.Y = np.vstack((dx, dy)).T
+        
+    def split_data(self):
+        """Separate data into training and testing sets with sklearn."""
+        if self.verbose: print("Splitting data into training and testing sets...")
+        self.Xtrain, self.Xtest, self.Ytrain, self.Ytest, self.Etrain, self.Etest = \
+            train_test_split(self.X, self.Y, self.E, test_size=self.test_size, random_state=self.random_state)
+    
+    def gen_White_Covariance(self):
+        """Generate white noise covariance matrix."""
+        if self.verbose: print("Generating white noise covariance function...")
+        self.W = np.diag(self.Etrain) + self.eps * np.eye(self.Etrain.shape[0])
+        self.Wss = np.diag(self.Etest) + self.eps * np.eye(self.Etest.shape[0])
+    
+    def __init__(self, datafile, nExposure, sample=None, verbose=False, eps=1.49e-8, test_size=0.20, random_state=None):
+        self.datafile = datafile        
+        self.nExposure = nExposure
+        self.sample = sample
+        self.verbose = verbose
+        self.eps = eps
+        self.test_size = test_size
+        self.random_state = random_state
+        
+        self.extract_exposure()
+        self.extract_data()
+        self.split_data()
+        self.gen_White_Covariance()
+    
+    def EBF(self, data, theta):
+        uu1 = data[0]
+        uu2 = data[1]
+        vv1 = data[2]
+        vv2 = data[3]
+        
+        var_s = theta[0]
+        sigma_x = theta[1]
+        sigma_y = theta[2]
+        phi = theta[3]
+        
+        a = np.cos(phi)**2 / (2 * sigma_x**2) + np.sin(phi)**2 / (2 * sigma_y**2)
+        b = - np.sin(2 * phi) / (4 * sigma_x**2) + np.sin(2 * phi) / (4 * sigma_y**2)
+        c = np.sin(phi)**2 / (2 * sigma_x**2) + np.cos(phi)**2 / (2 * sigma_y**2)
+        
+        uu = a * (uu1 - uu2)**2
+        vv = c * (vv1 - vv2)**2
+        uv = b * (uu1 - uu2)*(vv1 - vv2)
+        
+        return var_s * np.exp(-(uu + vv + uv))
+    
+    def gen_coordinate_arrays(self, X, Y):
+        u1, u2 = X[:, 0], Y[:, 0]
+        v1, v2 = X[:, 1], Y[:, 1]
+        
+        uu1, uu2 = np.meshgrid(u1, u2)
+        vv1, vv2 = np.meshgrid(v1, v2)
+        
+        return uu1, uu2, vv1, vv2
+        
+    def gen_RBF_Covariance(self, theta):
+        """Generate relevant covariance matrices."""
+        if self.verbose: print("Generating elliptical covariance function...")
+        data = self.gen_coordinate_arrays(self.Xtrain, self.Xtrain)
+        self.K = self.EBF(data, theta)
+        data = self.gen_coordinate_arrays(self.Xtest, self.Xtest)
+        self.Kss = self.EBF(data, theta)
+        data = self.gen_coordinate_arrays(self.Xtest, self.Xtrain)
+        self.Ks = self.EBF(data, theta)
+        
+    def train(self):
+        if self.verbose: print("Solving for posterior...")
+        t0 = time()
+        # The following commented-out code solves for the posterior distribution but
+        # does not involve cholesky decomposition (it simply uses np.linalg.inv) and is therefore
+        # computationally slower than the following block of code that does use np.linalg.cholesky
+        # (and by extension, it also uses np.linalg.solve.).
+        # KW_inv = np.linalg.inv(self.K + self.W)
+        # self.fbar_s = (self.Ks.T).dot(KW_inv).dot(self.Ytrain)
+        # self.V_s = self.Kss - (self.Ks.T).dot(KW_inv).dot(self.Ks)
+        # self.sigma = np.sqrt(np.abs(np.diag(self.V_s)))
+        
+        self.L = np.linalg.cholesky(self.K + self.W)
+        self.alpha = np.linalg.solve(self.L.T, np.linalg.solve(self.L, self.Ytrain))
+        self.fbar_s = np.dot(self.Ks.T, self.alpha)
+
+        self.v = np.linalg.solve(self.L, self.Ks)
+        self.V_s = self.Kss - np.dot(self.v.T, self.v)
+        self.sigma = np.sqrt(np.abs(np.diag(self.V_s)))
+        tf = time()
+        if self.verbose: print(f"Posterior found in {np.round(tf-t0, 3)} seconds.\n")
+
+    def fit(self, theta):
+        self.gen_RBF_Covariance(theta)
+        self.train()
+    
+    def summary(self):
+        print(f"Current Log Marginal Likelihood: {self.get_LML()}")
+        self.get_std()
+        self.plot_uv()
+        self.plot_residuals()
+        self.plot_closeup()
+        self.plot_quiver_GP()
+        self.plot_quiver_fits()
+        self.plot_resres()
+    
+    def draw_posterior(self, size=1):
+        """Draw from the posterior distribution."""
+        dx = np.random.multivariate_normal(self.fbar_s[:, 0], self.V_s, size=size).T
+        dy = np.random.multivariate_normal(self.fbar_s[:, 1], self.V_s, size=size).T
+        return dx, dy
+    
+    def draw_prior(self, size=1):
+        """Draw from the prior distribution."""
+        dx = np.random.multivariate_normal(np.zeros(self.Xtest.shape[0]), self.Kss + self.Wss, size=size).T
+        dy = np.random.multivariate_normal(np.zeros(self.Xtest.shape[0]), self.Kss + self.Wss, size=size).T
+        return dx, dy
+    
+    def get_LML(self):
+        """Calculates the log marginal likelihood based on the current posterior predictive
+        mean and the current posterior predictive variance."""
+        LML = (-1/2) * np.dot(self.Ytrain.T, self.alpha) - np.sum(np.log(np.diag(self.L))) - (self.Ytest.shape[0] / 2) * np.log(2 * np.pi)
+        return np.sum(np.diag(LML))
+    
+    def get_std(self):
+        std0_dx = np.std(self.Ytest[:, 0])
+        std0_dy = np.std(self.Ytest[:, 1])
+        stdf_dx = np.std(self.fbar_s[:, 0])
+        stdf_dy = np.std(self.fbar_s[:, 1])
+        improvement_dx = std0_dx / stdf_dx
+        improvement_dy = std0_dy / stdf_dy
+        print(f"Standard deviation of validation residuals: dx {np.round(std0_dx, 3)}, dy {np.round(std0_dy, 3)}")
+        print(f"Standard deviation of Gaussian Process residuals: dx {np.round(stdf_dx, 3)}, dy {np.round(stdf_dy, 3)}")
+        print(f"The ratio of validation residuals over GP residuals is: dx {np.round(improvement_dx, 3)}, dy {np.round(improvement_dy, 3)}")
+              
+    def plot_uv(self):
+        plt.figure(figsize=(8, 8))
+        plt.title(f"u, v Positions")
+        plt.xlabel("u (deg)")
+        plt.ylabel("v (deg)")
+        plt.scatter(self.Xtrain[:, 0], self.Xtrain[:, 1], alpha=0.5, label="Training Positions")
+        plt.scatter(self.Xtest[:, 0], self.Xtest[:, 1], alpha=0.5, label="Testing Positions")
+        plt.legend()
+        plt.show()
+              
+    def plot_residuals(self):
+        plt.figure(figsize=(8, 8))
+        plt.title(f"dx, dy Residuals")
+        plt.xlabel("dx (mas)")
+        plt.ylabel("dy (mas)")
+        plt.xlim((-100, 100))
+        plt.ylim((-100, 100))
+        plt.scatter(self.Ytrain[:, 0], self.Ytrain[:, 1], alpha=0.5, label="Training Residuals")
+        plt.scatter(self.Ytest[:, 0], self.Ytest[:, 1], alpha=0.5, label="Testing Residuals")
+        plt.scatter(self.fbar_s[:, 0], self.fbar_s[:, 1], alpha=0.5, label="Posterior Predictive Mean of Residuals")
+        plt.legend()
+        plt.show()
+              
+    def plot_closeup(self):
+        plt.figure(figsize=(8, 8))
+        plt.title("Closeup of Posterior Predictive Mean of Residuals with 1$\sigma$ Error Bars")
+        plt.xlabel("dx (mas)")
+        plt.ylabel("dy (mas)")
+        # plt.xlim((-100, 100))
+        # plt.ylim((-100, 100))
+        plt.errorbar(
+            self.fbar_s[:, 0],
+            self.fbar_s[:, 1],
+            xerr=1*self.sigma,
+            yerr=1*self.sigma,
+            fmt='o',
+            color='green',
+            alpha=0.5)
+        plt.show()
+    
+    def plot_quiver_GP(self):
+        plt.figure(figsize=(8, 8))
+        plt.title("Quiver Plot of Error for the Test Set with Errors given by the GP")
+        plt.quiver(self.Xtest[:, 0], self.Xtest[:, 1], self.fbar_s[:, 0], self.fbar_s[:, 1])
+        plt.show()
+              
+    def plot_quiver_fits(self):
+        plt.figure(figsize=(8, 8))
+        plt.title("Quiver Plot of Error for the Test Set with Errors given by the fits file")
+        plt.quiver(self.Xtest[:, 0], self.Xtest[:, 1], self.Ytest[:, 0], self.Ytest[:, 1])
+        plt.show()
+    
+    def plot_resres(self):
+        plt.figure(figsize=(8, 8))
+        plt.title(f"Test Set Residuals (given by fits file) Subtracted by Estimated Residuals (given by GP).")
+        plt.xlabel("dx (mas)")
+        plt.ylabel("dy (mas)")
+        plt.xlim((-100, 100))
+        plt.ylim((-100, 100))
+        plt.scatter(self.Ytest[:, 0] - self.fbar_s[:, 0], self.Ytest[:, 1] - self.fbar_s[:, 1], alpha=0.5)
+        plt.show()
---
+++ mcmc.py
@@ -0,0 +1,134 @@
+from time import time
+
+import numpy as np
+import astropy.units as u
+import astropy.io.fits as pf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+import forAustin as fa
+import gpr
+
+class MCMC(object):
+    def __init__(self, log_likelihood, data, theta, step_size, names=None, seed=314159):
+        self.log_likelihood = log_likelihood
+        self.data = data
+        self.theta = np.array(theta)
+        self.nparams = len(theta)
+        self.step_size = np.array(step_size)
+        self.rng = np.random.RandomState(seed)
+        self.naccept = 0
+        self.current_loglike = log_likelihood(self.data, self.theta)
+        self.samples = []
+        if names is None:
+            names = ["Parameter {:d}".format(k+1) for k in range(self.nparams)]
+        self.names = names      
+        
+        self.theta_news = np.array([0, 0, 0, 0])
+
+    def step(self, save=True):
+        """Take a single step in the chain"""
+        theta_new =  self.rng.normal(loc=self.theta, scale=self.step_size, size=self.theta.shape[0])
+        while np.any(theta_new < 0):
+            theta_new =  self.rng.normal(loc=self.theta, scale=self.step_size, size=self.theta.shape[0])
+        self.theta_news = np.vstack((self.theta_news, theta_new))
+        p_new = self.log_likelihood(self.data, theta_new)
+        ratio = np.exp(p_new - self.current_loglike)
+
+        if ratio >= 1.0:
+            take_step = 1
+        else:
+            stepran = self.rng.uniform()
+            if stepran < ratio:
+                take_step = 1
+            else:
+                take_step = 0
+
+        if take_step:
+            self.current_loglike = p_new
+            self.theta = theta_new
+
+        if save:
+            self.samples.append(self.theta)
+            
+        if save and take_step:
+            self.naccept += 1
+        
+    def burn(self, nburn):
+        """Take nburn steps, but don't save the results"""
+        for i in range(nburn):
+            self.step(save=False)
+
+    def run(self, nsteps):
+        """Take nsteps steps"""
+        for i in range(nsteps):
+            self.step()
+
+    def accept_fraction(self):
+        """Returns the fraction of candidate steps that were accpeted so far."""
+        if len(self.samples) > 0:
+            return float(self.naccept) / len(self.samples)
+        else:
+            return 0.
+        
+    def clear(self, step_size=None, theta=None):
+        """Clear the list of stored samples from any runs so far.
+        
+        You can also change the step_size to a new value at this time by giving a step_size as an
+        optional parameter value.
+        
+        In addition, you can reset theta to a new starting value if theta is not None.
+        """
+        if step_size is not None:
+            assert len(step_size) == self.nparams
+            self.step_size = np.array(step_size)
+        if theta is not None:
+            assert len(theta) == self.nparams
+            self.theta = np.array(theta)
+            self.current_loglike = self.log_likelihood(self.data, self.theta)
+        self.samples = []
+        self.naccept = 0
+        
+    def get_samples(self):
+        """Return the sampled theta values at each step in the chain as a 2d numpy array."""
+        return np.array(self.samples)
+        
+    def plot_hist(self):
+        """Plot a histogram of the sample values for each parameter in the theta vector."""
+        all_samples = self.get_samples()
+        for k in range(self.nparams):
+            theta_k = all_samples[:,k]
+            plt.hist(theta_k, bins=100)
+            plt.xlabel(self.names[k])
+            plt.ylabel("N Samples")
+            plt.show()
+        
+    def plot_samples(self):
+        """Plot the sample values over the course of the chain so far."""
+        all_samples = self.get_samples()
+        for k in range(self.nparams):
+            theta_k = all_samples[:,k]
+            plt.plot(range(len(theta_k)), theta_k)
+            plt.xlabel("Step in chain")
+            plt.ylabel(self.names[k])
+            plt.show()
+
+    def calculate_mean(self, weight=None):
+        """Calculate the mean of each parameter according to the samples taken so far.
+        
+        Optionally, provide a weight array to weight the samples.
+        
+        Returns the mean values as a numpy array.
+        """
+
+        return np.average(np.array(self.samples), axis=0, weights=weight)
+    
+    def calculate_cov(self, weight=None):
+        """Calculate the covariance matrix of the parameters according to the samples taken so far.
+
+        Optionally, provide a weight array to weight the samples.
+        
+        Returns the covariance matrix as a 2d numpy array.
+        """
+
+        return np.cov(np.array(self.samples).T, fweights=weight)
